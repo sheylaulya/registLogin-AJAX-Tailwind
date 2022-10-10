478db074bccb1070e0863631e3f0f9856c560f17
sheylaulya <sheylaulyaa@gmail.com>
2022-10-11 03:56:09 +0700

    tailwindcss

--- login.php
+++ login.php
@@ -20,12 +20,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <form method="post" action="login.php">
         <input type="text" id="username" placeholder="Username..."> <br>
         <input type="password" id="password" placeholder="Password..."> <br>
         <input type="button" value="Log In" id="login">
+
     
     </form>  
 
--- regist.php
+++ regist.php
@@ -21,34 +21,35 @@
 <head>
     <meta charset="utf-8">
     <title>Register</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
     <h2>Register</h2>
     <form autocomplete="off" action="" method="post">
         <div class="firstName">
-            <label for="">First Name</label>
-            <input type="text" id="firstName" value=""> <br>
+            <label for="" class="block text-sm font-medium text-slate-900">First Name</label>
+            <input type="text" id="firstName" value="" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-1/3 rounded-md sm:text-sm focus:ring-1"> 
         </div>
         <div class="lastName">
-            <label for="">Last Name</label>
-            <input type="text" id="lastName" value=""> <br>
+            <label for="" class="block text-sm font-medium text-slate-900">Last Name</label>
+            <input type="text" id="lastName" value="" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-1/3 rounded-md sm:text-sm focus:ring-1"> 
         </div>
         <div class="email">
-            <label for="">Email</label>
-            <input type="text" id="email" value=""> <br>
+            <label for="" class="block text-sm font-medium text-slate-900">Email</label>
+            <input type="text" id="email" value="" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-1/3 rounded-md sm:text-sm focus:ring-1">
         </div>
         <div class="username">
-            <label for="">Username</label>
-            <input type="text" id="username" value=""> <br>
+            <label for="" class="block text-sm font-medium text-slate-900">Username</label>
+            <input type="text" id="username" value="" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-1/3 rounded-md sm:text-sm focus:ring-1">
         </div>
         <div class="password">
-            <label for="">Password</label>
-            <input type="password" id="password" value=""> <br>
+            <label for="" class="block text-sm font-medium text-slate-900">Password</label>
+            <input type="password" id="password" value="" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-1/3 rounded-md sm:text-sm focus:ring-1">
 
         </div>
 
-        <input type="button" value="regist" id="regist">
+        <input type="button" value="regist" id="regist" class="px-5 py-1 font-bold text-sm bg-cyan-500 text-white rounded-full shadow-sm">
     </form>
 
     <p id="response"></p>
