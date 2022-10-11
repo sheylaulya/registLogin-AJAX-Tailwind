786e9f3e0d7afb24ce2445ca6d280e79da481c81
sheylaulya <sheylaulyaa@gmail.com>
2022-10-11 11:12:35 +0700

    tailwind

--- regist.php
+++ regist.php
@@ -18,47 +18,81 @@
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
 <head>
     <meta charset="utf-8">
     <title>Register</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Caveat+Brush&display=swap" rel="stylesheet">
 </head>
 
-<body>
-    <h2>Register</h2>
-    <form autocomplete="off" action="" method="post">
-        <div class="firstName">
-            <label for="" class="block text-sm font-medium text-slate-900">First Name</label>
-            <input type="text" id="firstName" value="" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-1/3 rounded-md sm:text-sm focus:ring-1"> 
-        </div>
-        <div class="lastName">
-            <label for="" class="block text-sm font-medium text-slate-900">Last Name</label>
-            <input type="text" id="lastName" value="" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-1/3 rounded-md sm:text-sm focus:ring-1"> 
-        </div>
-        <div class="email">
-            <label for="" class="block text-sm font-medium text-slate-900">Email</label>
-            <input type="text" id="email" value="" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-1/3 rounded-md sm:text-sm focus:ring-1">
-        </div>
-        <div class="username">
-            <label for="" class="block text-sm font-medium text-slate-900">Username</label>
-            <input type="text" id="username" value="" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-1/3 rounded-md sm:text-sm focus:ring-1">
-        </div>
-        <div class="password">
-            <label for="" class="block text-sm font-medium text-slate-900">Password</label>
-            <input type="password" id="password" value="" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-1/3 rounded-md sm:text-sm focus:ring-1">
+<style>
+    * {
+        font-family: 'Caveat Brush', cursive;
+    }
+</style>
 
+<body
+    class="bg-[url('https://img.freepik.com/free-vector/cartoon-jungle-background_23-2148962077.jpg?w=1800&t=st=1665457262~exp=1665457862~hmac=486f95b18fa307f36155f03bd4c3801e253eb38dc46ad905359ec1d4acad0ff8')]">
+    <div
+        class="bg w-[450px] h-[580px] bg-[#42855B] mx-auto mt-[100px] rounded-2xl flex flex-col justify-center items-center">
+        <div class="text-title text-center mt-[20px] mb-[20px] text-3xl font-bold tracking-widest text-[#FAF7F0]">
+            <h5>Create an account</h5>
         </div>
 
-        <input type="button" value="regist" id="regist" class="px-5 py-1 font-bold text-sm bg-cyan-500 text-white rounded-full shadow-sm">
-    </form>
+        <form autocomplete="off" action="" method="post">
+            <div class="firstName">
+                <label for="" class="block text-lg text-[#FAF7F0] font-bold tracking-wide font-'Caveat'">First
+                    Name</label>
+                <input type="text" id="firstName" value=""
+                    class="w-80 px-4 py-1 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+            </div>
+            <div class="lastName">
+                <label for="" class="block text-lg  text-[#FAF7F0]  font-bold tracking-wide">Last Name</label>
+                <input type="text" id="lastName" value=""
+                    class="w-80 px-4 py-1 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+            </div>
+            <div class="email">
+                <label for="" class="block text-lg text-[#FAF7F0]  font-bold tracking-wide">Email</label>
+                <input type="text" id="email" value=""
+                    class="w-80 px-4 py-1 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+            </div>
+            <div class="username">
+                <label for="" class="block text-lg text-[#FAF7F0]  font-bold tracking-wide">Username</label>
+                <input type="text" id="username" value=""
+                    class="w-80 px-4 py-1 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+            </div>
+            <div class="password">
+                <label for="" class="block text-lg text-[#FAF7F0]  font-bold tracking-wide">Password</label>
+                <input type="password" id="password" value=""
+                    class="w-80 px-4 py-1 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
 
-    <p id="response"></p>
+            </div>
+
+            <input type="button" value="regist" id="regist"
+                class="px-5 py-1 mt-4  mb-5 text-white bg-[#425F57] rounded-lg hover:bg-[#749F82]">
+            <input type="reset" value="reset"
+            class="px-5 py-1 mt-4  mb-5 text-white bg-[#E26868] rounded-lg hover:bg-[#749F82]">
+        </form>
+
+        <a href="login.php" class="tracking-widest font-bold mt-[-10px] text-[#FAF7F0]  ">Have an account already? Sign
+            In</a>
+
+        <p id="response"></p>
+    </div>
+    </div>
+    <div class="wrapper">
 
 
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    </div>
+
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script type="text/javascript">
-        $(document).ready(function (){
-            $("#regist").on('click', function(){
+        $(document).ready(function () {
+            $("#regist").on('click', function () {
                 var firstName = $("#firstName").val();
                 var lastName = $("#lastName").val();
                 var email = $("#email").val();
@@ -66,12 +100,17 @@
                 var password = $("#password").val();
 
 
-                if ( firstName == "" || lastName == "" || email == "" || username == "" || password == "")
-                alert('Please check your inputs');
-                else{
-                    
-                $.ajax(
-                    {
+                if (firstName == "" || lastName == "" || email == "" || username == "" || password ==
+                    "")
+                    swal({
+                        title: "Failed",
+                        text: "Please Check Your Inputs!",
+                        icon: "warning",
+                        button: "try again",
+                    });
+                else {
+
+                    $.ajax({
                         url: 'regist.php',
                         method: 'POST',
                         data: {
@@ -82,18 +121,18 @@
                             usernamePHP: username,
                             passwordPHP: password
                         },
-                        success : function (response) {
+                        success: function (response) {
                             $("#response").html(response);
                         },
-                        dataType : 'text '
-                    }
-                )
+                        dataType: 'text '
+                    })
                 }
 
 
             })
         })
     </script>
+
 </body>
 
 </html>
